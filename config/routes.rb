246ab3dda33fb31e0feb3feea251a56bad6503e4
rioyi <wilbert.angarita@gmail.com>
2017-05-25 21:15:36 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,7 @@
 Rails.application.routes.draw do
-
   root 'home#index'
-
   devise_for :users
-
-  get 'products/index'
-
-  get 'products/edit'
-
-  get 'products/new'
-
-  get 'products/show'
-
-  get 'stores/index'
-
-  get 'stores/new'
-
-  get 'stores/create'
-
-  get 'stores/show'
-
-  get 'stores/edit'
-
-  get 'stores/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :stores do 
+  resources :stores do
   	resources :products
   end
 end
